5dab4b84b84272e35959722f75a952ca699d2711
wallyson2712 <wallyson2712@gmail.com>
2013-05-04 11:49:35 -0300

    Mudado o pacote

---
+++ MeusEstudosJava/src/EntraDeDados.java
@@ -0,0 +1,26 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EntraDeDados {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String... args) throws IOException {
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		// System.out.println("Digite o valor do dia do seu nacimento");
+		// int nacimento = Integer.parseInt(bf.readLine());
+
+		// StringTokenizer st = new StringTokenizer("Esse é u teste");
+		// while (st.hasMoreTokens()) {
+		// System.out.println(st.nextToken());
+		// }
+		//
+	}
+
+}
